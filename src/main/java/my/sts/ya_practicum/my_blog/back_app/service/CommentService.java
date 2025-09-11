002999df854204c928ee947eba078d635adf1c66
Stas R <ratnikov.stanislav@gmail.com>
2025-09-10 21:30:41 -0700

    add null checks

--- src/main/java/my/sts/ya_practicum/my_blog/back_app/service/CommentService.java
+++ src/main/java/my/sts/ya_practicum/my_blog/back_app/service/CommentService.java
@@ -5,6 +5,7 @@ import my.sts.ya_practicum.my_blog.back_app.dto.CommentDto;
 import my.sts.ya_practicum.my_blog.back_app.util.mapper.CommentDtoMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -18,10 +19,18 @@ public class CommentService {
     }
 
     public List<CommentDto> findCommentsByPostId(Long postId) {
+        if (postId == null) {
+            return Collections.emptyList();
+        }
+
         return CommentDtoMapper.map(commentRepository.findByPostId(postId));
     }
 
     public Map<Long, Long> getCommentCounts(List<Long> postIds) {
+        if (postIds == null || postIds.isEmpty()) {
+            return Collections.emptyMap();
+        }
+
         return commentRepository.getCommentCounts(postIds);
     }
 }
