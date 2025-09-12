36ca00e31eb6c37369078c7856d96a97ab6b0f98
Stas R <ratnikov.stanislav@gmail.com>
2025-09-12 15:35:21 -0700

    CommentDtoMapper unit test

--- src/main/java/my/sts/ya_practicum/my_blog/back_app/model/Comment.java
+++ src/main/java/my/sts/ya_practicum/my_blog/back_app/model/Comment.java
@@ -1,8 +1,14 @@
 package my.sts.ya_practicum.my_blog.back_app.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @Setter
 public class Comment {
---
+++ src/test/java/my/sts/ya_practicum/my_blog/back_app/util/mapper/CommentDtoMapperTest.java
@@ -0,0 +1,57 @@
+package my.sts.ya_practicum.my_blog.back_app.util.mapper;
+
+import my.sts.ya_practicum.my_blog.back_app.dto.CommentDto;
+import my.sts.ya_practicum.my_blog.back_app.model.Comment;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CommentDtoMapperTest {
+
+    private static final Comment testComment = Comment.builder()
+            .id(123L)
+            .postId(42L)
+            .text("comment_text")
+            .build();
+
+    @Test
+    public void shouldReturnNull_whenCommentIsNull() {
+        assertNull(CommentDtoMapper.map((Comment) null));
+    }
+
+    @Test
+    public void shouldReturnEmptyList_whenCommentsAreNull() {
+        List<CommentDto> result = CommentDtoMapper.map((List<Comment>) null);
+
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnValidResult_whenCommentIsValid() {
+        validateResult(testComment, CommentDtoMapper.map(testComment));
+    }
+
+    @Test
+    public void shouldReturnValidResultList_whenCommentsAreValid() {
+        List<CommentDto> result = CommentDtoMapper.map(List.of(testComment));
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+
+        validateResult(testComment, result.get(0));
+    }
+
+    private void validateResult(Comment comment, CommentDto result) {
+        assertNotNull(result);
+
+        assertEquals(comment.getId(), result.getId());
+        assertEquals(comment.getPostId(), result.getPostId());
+        assertEquals(comment.getText(), result.getText());
+    }
+}
