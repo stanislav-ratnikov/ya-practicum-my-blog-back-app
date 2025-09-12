a8a1703a15022a06d5710ad55b1e5d740a4b4e30
Stas R <ratnikov.stanislav@gmail.com>
2025-09-12 14:54:55 -0700

    PostDtoMapper unit tests

--- src/main/java/my/sts/ya_practicum/my_blog/back_app/model/Post.java
+++ src/main/java/my/sts/ya_practicum/my_blog/back_app/model/Post.java
@@ -1,10 +1,16 @@
 package my.sts.ya_practicum.my_blog.back_app.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @Setter
 public class Post {
