103c6e0b99d72da59b5680e3977664e05dd42661
Stas R <ratnikov.stanislav@gmail.com>
2025-09-14 23:11:17 -0700

    PostService unit test

---
+++ src/test/java/my/sts/ya_practicum/my_blog/back_app/service/PostServiceTest.java
@@ -0,0 +1,131 @@
+package my.sts.ya_practicum.my_blog.back_app.service;
+
+import my.sts.ya_practicum.my_blog.back_app.dao.PostRepository;
+import my.sts.ya_practicum.my_blog.back_app.dto.FindPostsResponseDto;
+import my.sts.ya_practicum.my_blog.back_app.dto.PostDto;
+import my.sts.ya_practicum.my_blog.back_app.model.Post;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PostServiceTest {
+
+    private static final Post testPost = Post.builder()
+            .id(1L)
+            .title("title")
+            .text("text")
+            .likesCount(1L)
+            .tags(List.of("tag1", "tag2"))
+            .build();
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private CommentService commentService;
+
+    @InjectMocks
+    private PostService postService;
+
+    @Test
+    void findPosts_shouldReturnValidResponseDto_whenPostsFound() {
+        when(postRepository.find(any(), any(), any())).thenReturn(List.of(testPost));
+        when(postRepository.countTotalPosts(any())).thenReturn(1);
+        when(commentService.getCommentsCountByPostId(any())).thenReturn(new HashMap<>(){
+            {
+                put(testPost.getId(), 1L);
+            }
+        });
+
+        FindPostsResponseDto responseDto = postService.findPosts("поисковая строка", 1, 5);
+
+        assertEquals(false, responseDto.getHasNext());
+        assertEquals(false, responseDto.getHasPrev());
+        assertEquals(1, responseDto.getLastPage());
+
+        assertNotNull(responseDto);
+
+        List<PostDto> posts = responseDto.getPosts();
+
+        assertNotNull(posts);
+        assertEquals(1, posts.size());
+
+        PostDto postDto = posts.getFirst();
+
+        validate(postDto, testPost);
+    }
+
+    @Test
+    void findById_shouldReturnPostDto_whenPostFound() {
+        when(postRepository.findById(1L)).thenReturn(testPost);
+        when(commentService.getCommentsCountByPostId(List.of(1L))).thenReturn(new HashMap<>(){
+            {
+                put(1L, 1L);
+            }
+        });
+
+        PostDto postDto = postService.findById(1L);
+
+        validate(postDto, testPost);
+    }
+
+    @Test
+    void createPost_shouldReturnPostDto_whenSavedAndLoadedSuccessfully() {
+        when(postRepository.save(any())).thenReturn(1L);
+        when(postRepository.findById(1L)).thenReturn(testPost);
+        when(commentService.getCommentsCountByPostId(any())).thenReturn(new HashMap<>(){
+            {
+                put(testPost.getId(), 1L);
+            }
+        });
+
+        PostDto result = postService.createPost(new PostDto());
+
+        validate(result, testPost);
+    }
+
+    @Test
+    void updatePost_shouldReturnPostDto_whenUpdatedAndLoadedSuccessfully() {
+        when(postRepository.findById(1L)).thenReturn(testPost);
+        when(commentService.getCommentsCountByPostId(any())).thenReturn(new HashMap<>(){
+            {
+                put(testPost.getId(), 1L);
+            }
+        });
+
+        PostDto result = postService.updatePost(1L, new PostDto());
+
+        validate(result, testPost);
+    }
+
+    @Test
+    void incrementLikesCount_shouldReturnLikesCount_whenIncrementedSuccessfully() {
+        when(postRepository.incrementLikes(1L)).thenReturn(123L);
+
+        Long likesCount = postService.incrementLikesCount(1L);
+
+        assertNotNull(likesCount);
+        assertEquals(123L, likesCount);
+    }
+
+    private void validate(PostDto postDto, Post expected) {
+        assertNotNull(postDto);
+        assertEquals(expected.getId(), postDto.getId());
+        assertEquals(expected.getTitle(), postDto.getTitle());
+        assertEquals(expected.getText(), postDto.getText());
+        assertEquals(expected.getLikesCount(), postDto.getLikesCount());
+        assertIterableEquals(expected.getTags(), postDto.getTags());
+    }
+}
